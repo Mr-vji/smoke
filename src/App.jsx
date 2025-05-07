f077130c44a8548f8f49b124e991a61fc893a68a
Vji <mrvijaykumar.in@gmail.com>
2025-05-07 11:16:03 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Leva } from "leva";
+import { Loader } from "@react-three/drei";
 
 function App() {
    return (
-      <Canvas shadows camera={{ position: [0, 3, 15], fov: 30 }}>
-         <Experience />
-      </Canvas>
+      <>
+         <Leva collapsed={true} />
+         <Loader />
+         <Canvas shadows camera={{ position: [0, 3, 15], fov: 30 }}>
+            <Experience />
+         </Canvas>
+      </>
    );
 }
 
